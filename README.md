15ee368741fcdf410218ec1f6d4700f9a397752f
zyao89 <zyao89@gmail.com>
2019-04-22 22:51:55 +0800

    :add: README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# @2o3t-core/plugin-common
+
+2o3t-core 通用插件
+
+## usage
+
+在config目录下创建 `plugin.default.js` 文件, 输入一下内容引用:
+
+```js
+module.exports = {
+    mixins: [
+        '@2o3t-core/plugin-common',
+    ],
+};
+```
--- helper/cache.js
+++ helper/cache.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const DEV_TIME_CACHE = {};
+
 module.exports = function(app) {
     const Cache = {};
 
     const get = async key => {
         const redis = app.loadHelper.redis;
+        if (DEV_TIME_CACHE[key]) {
+            if (DEV_TIME_CACHE[key] < Date.now()) {
+                delete DEV_TIME_CACHE[key];
+                await Cache.clear(key);
+                return;
+            }
+        }
         const data = await redis.get(key);
         if (!data) {
             return data;
@@ -23,20 +32,26 @@ module.exports = function(app) {
         if (redis.setex) {
             return await redis.setex(key, time, value);
         }
+        DEV_TIME_CACHE[key] = Date.now() + time * 1000;
         return await redis.set(key, value);
     };
     Cache.set = set;
 
     const clear = async key => {
         const redis = app.loadHelper.redis;
+        delete DEV_TIME_CACHE[key];
         return await redis.del(key);
     };
     Cache.clear = clear;
 
     const clearAll = async key => {
         const redis = app.loadHelper.redis;
+        if (!redis.keys) return;
         const aKey = await redis.keys(key);
         if (!!aKey && aKey.length > 0) {
+            aKey.forEach(key => {
+                delete DEV_TIME_CACHE[key];
+            });
             return await redis.del(aKey);
         }
     };
@@ -44,8 +59,9 @@ module.exports = function(app) {
 
     const getAll = async key => {
         const redis = app.loadHelper.redis;
+        if (!redis.keys) return;
         const aKey = await redis.keys(key);
-        if (!!aKey && aKey.length > 0) {
+        if (!!aKey && aKey.length > 0 && redis.mget) {
             const datas = await redis.mget(aKey);
             if (datas && Array.isArray(datas)) {
                 return datas.map(data => JSON.parse(data));
